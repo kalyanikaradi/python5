9d4f9724c5302b6f968483a78ecece80abd36676
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2023-12-21 13:25:04 +0530

    Add files via upload

---
+++ 21 dec.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#tuple
+t1=(2,3,4,5,6)
+t2=(2.3,5.6,3.5)
+t3=('w','e',2.4,5)
+t4=('one','two','three')
+
+
+# In[3]:
+
+
+t1
+
+
+# In[4]:
+
+
+t2
+
+
+# In[5]:
+
+
+t3
+
+
+# In[6]:
+
+
+t4
+
+
+# In[7]:
+
+
+t5=('abc',45,'c',4.5)
+
+
+# In[8]:
+
+
+t5[2]
+
+
+# In[9]:
+
+
+t5[1]
+
+
+# In[10]:
+
+
+t5[6]
+
+
+# In[12]:
+
+
+t5[1:3]
+
+
+# In[13]:
+
+
+t5[0:5]
+
+
+# In[14]:
+
+
+del t3
+
+
+# In[15]:
+
+
+t3
+
+
+# In[26]:
+
+
+#tuple methods
+#count(returns number of count of specified value)
+t=(2,3,3,4,5,6,7,7,8)
+t
+
+
+# In[30]:
+
+
+t.count(7)
+
+
+# In[32]:
+
+
+t.count(3)
+
+
+# In[33]:
+
+
+t.count(9)
+
+
+# In[34]:
+
+
+#index(specify index value)
+t
+
+
+# In[35]:
+
+
+t.index(8)
+
+
+# In[36]:
+
+
+t.index(3)
+
+
+# In[37]:
+
+
+t.index(6)
+
+
+# 
